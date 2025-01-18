6cab03e431ea996684b39c3986d52a32b0418127
Pheto <27964574@sun.ac.za>
2025-01-19 00:44:49 +0200

    1st commit,no tests made

---
+++ ATM_Interface.java/src/BankAccount.java
@@ -0,0 +1,17 @@
+
+// Store the account balance
+// neeed to connect the users account and the ATM
+public class BankAccount {
+    
+    private int AccountBalance;
+
+    public int getAccountBalance() {
+        // Method returns the balance of the account
+        return AccountBalance;
+    }
+
+    public void setAccountBalance(int accountBalance) {
+        // Sets a new amount in the account
+        AccountBalance = accountBalance;
+    }
+}
